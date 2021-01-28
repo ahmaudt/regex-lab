71f4cc48b38e2c638b8fc38783ba203c2d0563bc
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-27 19:50:07 -0500

    commit tree 590487d56b8878578b318b00a78f1697008f033b

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,23 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+if word.match(/^[aeiouAEIOU]/)
+    return true
+    elsif
+        word.match(/^[^aeiouAEIOU]/)
+        return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    un_prefix_ing_suffix = []
 end
 
 def words_five_letters_long(text)
-
+#    puts text.kind_of?(String)
+   five_words = text.scan(/\b\w{5}\b/)
+   five_words
+    
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
